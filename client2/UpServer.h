f2acf47a473fdcccedc4c75dcd9263342c11f6a7
xihajuan2010 <xihajuan2010@gmail.com>
2017-04-28 23:52:43 +0800

    delete some file

--- client2/UpServer.h
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include "libtorrent/entry.hpp"
-#include "libtorrent/bencode.hpp"
-#include "libtorrent/session.hpp"
-
-class UpServer{
-	public:
-		static void* StartUp(void *args);
-};
